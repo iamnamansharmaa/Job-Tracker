82bada603fda1949dce300f1af3ce70f7d4ca940
iamnamansharmaa <86802887+iamnamansharmaa@users.noreply.github.com>
2025-04-19 16:29:48 +0530

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://itsnamansharma1:paaooVL5qUDAt8MF@jobtracker.uge0ouo.mongodb.net/?retryWrites=true&w=majority&appName=jobTracker', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  tlsAllowInvalidCertificates: true
 });
 
 module.exports = mongoose.connection;
